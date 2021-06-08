fd06d00fa859fafb1f48e32052bff0e1f5ee98dd
Cedric John <cedric.john@gmail.com>
2021-06-08 17:48:51 +0100

    Started the structure of the package and worked on a few notebooks

---
+++ marswinds/satellite_data.py
@@ -0,0 +1,11 @@
+import numpy as np
+import pandas as pd
+import os
+import ee
+import geemap
+
+def get_one_image(coordinates:list, *kwargs) -> str:
+    pass
+
+def get_region(data:pd.DataFrame) -> pd.DataFrame:
+    pass

4ca391356d06b3cbe79fd627f3877f44bb0b7b5d
Cedric John <cedric.john@gmail.com>
2021-06-10 15:32:51 +0100

    data fetching module fully functional

--- marswinds/prep_data.py
+++ marswinds/prep_data.py
@@ -1,15 +1,136 @@
 import pandas as pd
 import xarray as xr
 import numpy as np
+import math
+import glob, os
+from os import listdir
+from os.path import isfile, join
+import cv2
+from marswinds.satellite_data import SatelliteData
+from marswinds.wind_data import WindData
     
-    
-def prep_data(path):
+
+class DataPreparation:
+    def __init__(self,nb_lines=None, image_type='dunes', clean_download=False):
+        self.nb_lines=nb_lines
+        self.image_type=image_type
         
-    ds = xr.open_dataset(path)
-    raw_data = ds.to_dataframe()
-    clean_df = raw_data.dropna().reset_index()
-    clean_df["wind strength"] = (clean_df.u10**2 + clean_df.v10**2)**.5
-    clean_df['sin'] = np.sin(np.arctan2(clean_df['u10'],clean_df['v10']))
-    clean_df['cos'] = np.cos(np.arctan2(clean_df['u10'],clean_df['v10']))
-    wind_data = clean_df.groupby(by=['latitude','longitude']).mean().reset_index()
-    return wind_data
+        if clean_download:
+            os.rmdir(f'../raw_data/images/{self.image_type}')
+            
+        
+    
+    def fetch_all_data(self):
+        df = pd.read_csv(f'../raw_data/lists/{self.image_type}.csv')
+        
+        if self.nb_lines:
+            coordinates = df.head(self.nb_lines).copy()
+        else:
+            coordinates = df.copy()
+        
+        
+        for index in coordinates.index.values:
+            item = coordinates.iloc[index,:]
+            print('Now fetching:')
+            print(item)
+            wind_data = WindData(file_=item.name,
+                                 NWSE_coordinates = [item.north,
+                                  item.west,
+                                  item.south,
+                                  item.east],
+                                 image_type=self.image_type) \
+                        .get_data_one_region() \
+                        .prep_data()
+            
+            for data_index in wind_data.index.values:
+                data = wind_data.iloc[data_index,:]
+                SatelliteData(data).get_image_per_coordinates()
+        
+        return self                 
+    
+    def fetch_wind_only(self):
+        df = pd.read_csv(f'../raw_data/lists/{self.image_type}.csv')
+        
+        if self.nb_lines:
+            coordinates = df.head(self.nb_lines).copy()
+        else:
+            coordinates = df.copy()
+        
+        
+        for index in coordinates.index.values:
+            item = coordinates.iloc[index,:]
+            print(f'Now fetching {item.region}')
+            print(item)
+            wind_data = WindData(file_=item.region,
+                                 NWSE_coordinates = [item.north,
+                                  item.west,
+                                  item.south,
+                                  item.east],
+                                 image_type=self.image_type) \
+                        .get_data_one_region() \
+                        .prep_data()
+        return self
+    
+    def fetch_images_only(self, file_name=None):
+        wind_data = WindData(file_name).prep_data()
+        for data_index in wind_data.index.values:
+                data = wind_data.iloc[data_index,:]
+                SatelliteData(data).get_image_per_coordinates()
+        return self
+    
+    def rotate_images(self):
+        base_path = '../raw_data/image/training'
+        
+        mypath=f'{base_path}/{self.image_type}'
+        try:
+            list_of_images =  [f for f in listdir(mypath) if isfile(join(mypath, f))]
+            print(f'Now applying rotation to {len(list_of_images)} files')
+        except:
+            print(f'No rotation applied (check if the folder is empty)')
+            return self
+        
+        for image_name in list_of_images:
+            if image_name.split('.')[-1]=='jpg':
+                self.apply_rotation(f'{mypath}/{image_name}')
+        return self
+            
+    def apply_rotation(self, image_name):
+        image = cv2.imread(image_name)
+        rotation_values = [(cv2.cv2.ROTATE_90_CLOCKWISE,90,'CW090'),
+                           (cv2.cv2.ROTATE_180,180,'CW180'),
+                           (cv2.cv2.ROTATE_90_COUNTERCLOCKWISE,270,'CW270')]
+        
+        label = image_name.split('/')[-1]
+        original_sin = float(label.split('_')[-3])
+        original_cos = float(label.split('_')[-2])
+        original_strength = label.split('_')[-1]
+        original_angle = math.atan2(original_sin, original_cos)
+        original_angle *= 180 / math.pi
+        if original_angle < 0: original_angle += 360
+        
+        for rotation in rotation_values:
+            rota = rotation[1]
+            new_angle = original_angle + rota
+            if new_angle > 360: original_angle -= 360
+            new_sin = np.sin(new_angle)
+            new_cos = np.cos(new_angle)
+            name_tags = label.split('_')
+            name_tags[-4] = rotation[2]
+            name_tags[-3] = str(new_sin)
+            name_tags[-2] = str(new_cos)
+            image_label = "_".join(name_tags)
+            path_tags = image_name.split('/')
+            path_tags[-1] = image_label
+            image_path = '/'.join(path_tags)
+            new_image = cv2.rotate(image, rotation[0])
+            cv2.imwrite(image_path,new_image)
+        return self
+            
+        
+    
+    
+if __name__ == '__main__':
+    data_handler = DataPreparation(1, image_type='dunes')
+    data_handler.fetch_all_data().rotate_images()
+    #data_handler.fetch_images_only(0)
+    
--- marswinds/satellite_data.py
+++ marswinds/satellite_data.py
@@ -6,78 +6,76 @@ import geemap
 import cv2
 import xarray as xr
 
-def get_region(data:pd.DataFrame) -> pd.DataFrame:
-    pass
-
-def get_image_per_coordinates(data:pd.Series, **kwargs) -> list:
+class SatelliteData:
     
-        coordinates = [(data.longitude-0.025,data.latitude-0.025),
-              (data.longitude-0.025,data.latitude+0.025),
-              (data.longitude+0.025,data.latitude+0.025),
-              (data.longitude+0.025,data.latitude-0.025)]
+    def __init__(self, data):
+        self.data = data 
+            
+    def get_image_per_coordinates(self, **kwargs) -> list:
+    
+            coordinates = [
+              (self.data.longitude+0.025,self.data.latitude+0.025,1),
+              (self.data.longitude-0.025,self.data.latitude+0.025,2),
+              (self.data.longitude-0.025,self.data.latitude-0.025,3),
+              (self.data.longitude+0.025,self.data.latitude-0.025,4)]
         
-        for quadrant,coordinate in enumerate(coordinates):
-            get_one_image(long=coordinate[0], lat=coordinate[1], data=data, quadrant=quadrant)
+            for coordinate in coordinates:
+                self.get_one_image(long=coordinate[0], lat=coordinate[1], data=self.data, quadrant=coordinate[2])
         
-        return None
+            return None
     
-def get_one_image(self,long:float, lat:float,data:pd.Series, quadrant:int, **kwargs) -> list:
+    def get_one_image(self,long:float, lat:float,quadrant:int, **kwargs) -> list:
     
-    ee.Initialize()
+        ee.Initialize()
     
-    if 'resolution' in kwargs.keys():
-        resolution=int(kwargs['resolution'])
-    else:
-        resolution = 512
+        if 'resolution' in kwargs.keys():
+            resolution=int(kwargs['resolution'])
+        else:
+            resolution = 512
     
-    satellite_name='COPERNICUS/S2_SR'
+        satellite_name='COPERNICUS/S2_SR'
     
-    area = [(long-0.025,lat-0.025),
-            (long+0.025,lat+0.025)]
-        
-    roi = ee.Geometry.Rectangle(coords=area)
+        area = [(long-0.025,lat-0.025),
+                (long+0.025,lat+0.025)]
+        print(f'Fetching {lat}/{long}')
+        roi = ee.Geometry.Rectangle(coords=area)
            
-    collection = ee.ImageCollection(satellite_name) \
+        collection = ee.ImageCollection(satellite_name) \
                 .filterBounds(roi) \
-                .sort("CLOUD_COVER") \
-                .filter('HIGH_PROBA_CLOUDS_PERCENTAGE < 10') \
+                .filter('HIGH_PROBA_CLOUDS_PERCENTAGE < 1') \
+                .filter('NODATA_PIXEL_PERCENTAGE < 1') \
                 .limit(1)
 
-    image = collection.first()
+        image = collection.first()
     
-    vis_params = {
+        vis_params = {
                   'bands': [ 'B4','B3','B2'],
                   'min': 0,
                   'max': 10000,
                   'gamma': 1.4}
 
-    file_name = f"{lat}_{long}_0{quadrant}_CW000_{data.sin}_{data.cosin}_{data.wind_strength}"
-    out_img = os.path.expanduser(f"../raw_data/practice_dataset/{file_name}.jpg")
+        image_name = f"{lat}_{long}_0{quadrant}_CW000_{self.data.sin}_{self.data.cos}_{self.data.wind_strength}"
+        file_name = f"../raw_data/images/{self.data.folder}/{self.data.image_type}/{image_name}.jpg"
+        out_img = os.path.expanduser(file_name)
 
-    geemap.get_image_thumbnail(image, out_img,vis_params,dimensions=(resolution, resolution),region=roi, format='jpg')
-    image_grey = cv2.cvtColor(cv2.imread(out_img), 
+        try:
+            geemap.get_image_thumbnail(image, out_img,vis_params,dimensions=(resolution, resolution),region=roi, format='jpg')
+            image_grey = cv2.cvtColor(cv2.imread(out_img), 
                               cv2.COLOR_BGR2GRAY)
-    cv2.imwrite(f"../raw_data/practice_dataset/{file_name}.jpg",
-                image_grey)
+            cv2.imwrite(file_name,image_grey)
+        except:
+            print('No image corresponding to filter criteria')
         
-    return None
+        
+        return None
 
-def prep_data(path):
-    ds = xr.open_dataset(path)
-    raw_data = ds.to_dataframe()
-    clean_df = raw_data.dropna().reset_index()
-    clean_df['wind_strength'] = (clean_df.u10**2 + clean_df.v10**2)**.5
-    clean_df['sin'] = np.sin(np.arctan2(clean_df['u10'],clean_df['v10']))
-    clean_df['cosin'] = np.cos(np.arctan2(clean_df['u10'],clean_df['v10']))
-    wind_data = clean_df.groupby(by=['latitude','longitude']).mean().reset_index()
-    return wind_data
+    def prep_data(self, path):
+        ds = xr.open_dataset(path)
+        raw_data = ds.to_dataframe()
+        clean_df = raw_data.dropna().reset_index()
+        clean_df['wind_strength'] = (clean_df.u10**2 + clean_df.v10**2)**.5
+        clean_df['sin'] = np.sin(np.arctan2(clean_df['u10'],clean_df['v10']))
+        clean_df['cosin'] = np.cos(np.arctan2(clean_df['u10'],clean_df['v10']))
+        wind_data = clean_df.groupby(by=['latitude','longitude']).mean().reset_index()
+        return wind_data
 
-if __name__ == '__main__':
-    file_paths = ['../raw_data/practice_wind_dataset.nc',
-                  '../raw_data/white_sands_winds.nc',
-                  '../raw_data/practice_wind_dataset.nc',
-                  '../raw_data/gobi_winds.nc']
-    
-    for path in file_paths:
-        test_data = prep_data(path).iloc[1,:]
-        get_image_per_coordinates(test_data)
